b0b90ef7113acc2d890f3d06386c50b1c283ca4c
EMMANUEL ATSU <zioncodex@gmail.com>
2023-09-17 21:25:59 +0100

    commit

--- resources/views/pages/product_view_table_index.blade.php
+++ resources/views/pages/product_view_table_index.blade.php
@@ -89,15 +89,7 @@
             
                 <td>{{ $loop->iteration }}</td>
 
-                <img src="{{ public_path('images/drug.jpg')}}" alt="" />
-                <img src="https://admin.accessmydrugs.com/public/storage/images/drug.jpg" style="width: 10%; height: 10%;">
-
-                <img src="{{ url('storage/app/public/product_image/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('storage/app/product_image/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/storage/app/public/images/product_image/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/storage/images/product_image/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/public/storage/app/public/images/drug.jpg') }}" alt="" title="" />
-
+      
                 <td>
                     {{-- IMAGE BOX STARTS --}}
                         @if (empty($product->product_image))
