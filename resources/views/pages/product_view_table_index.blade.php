385ade4b351766d45a14c1f6637b397a944434fb
EMMANUEL ATSU <zioncodex@gmail.com>
2023-09-17 20:27:46 +0100

    commit

--- resources/views/pages/product_view_table_index.blade.php
+++ resources/views/pages/product_view_table_index.blade.php
@@ -91,14 +91,14 @@
         
 
 
-                <img src="https://admin.accessmydrugs.com/public/storage/images/drug.jpg" style="width: 100%; height: 100%;">
+                <img src="https://admin.accessmydrugs.com/public/storage/product_image/drug.jpg" style="width: 100%; height: 100%;">
 
                 <img src="{{ url('storage/app/public/product_image/drug.jpg') }}" alt="" title="" />
                 <img src="{{ url('storage/app/product_image/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/storage/app/public/images/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/storage/images/drug.jpg') }}" alt="" title="" />
-                <img src="{{ url('https://admin.accessmydrugs.com/public/storage/app/public/images/drug.jpg') }}" alt="" title="" />
-                
+                <img src="{{ url('https://admin.accessmydrugs.com/storage/app/public/product_image/drug.jpg') }}" alt="" title="" />
+                <img src="{{ url('https://admin.accessmydrugs.com/storage/product_image/drug.jpg') }}" alt="" title="" />
+                <img src="{{ url('https://admin.accessmydrugs.com/public/storage/app/public/product_image/drug.jpg') }}" alt="" title="" />
+
                 <td>
                     {{-- IMAGE BOX STARTS --}}
                         @if (empty($product->product_image))
