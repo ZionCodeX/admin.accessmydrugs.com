74a31f68e395a990ddf0c6cfc34f09bff8d8f5c5
EMMANUEL ATSU <zioncodex@gmail.com>
2023-12-02 19:56:02 +0100

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,7 +53,7 @@ class ProductController extends Controller
 
         // Download image - this does not handle 404 or other webserver errors
         $imageContent = file_get_contents($url);
-        \Illuminate\Support\Facades\Storage::disk('public/images')->put($image, $imageContent);
+        \Illuminate\Support\Facades\Storage::disk('bulk')->put($image, $imageContent);
 
         dd("Image 3 Uploaded OK!!!");
 
