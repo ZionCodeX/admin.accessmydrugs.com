b73b30e4eee59b6fa2389c56496c4a48b95b357a
EMMANUEL ATSU <zioncodex@gmail.com>
2023-12-03 19:55:21 +0100

    commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -24,10 +24,10 @@ class MailController extends Controller
             $data['mail_template'] = $xdata["mail_template"];
         
             //send mail
-            \Mail::to($data["to"])->send(new SendMail($data));
+            Mail::to($data["to"])->send(new SendMail($data));
 
             //check and respond to mail request
-            if (\Mail::failures()) 
+            if (Mail::failures()) 
             {   
                 //dd('failed');
                 $send_status = 'SUCCESS';
