3d48c28f076cced4ae0b543221a048b7a8fce766
EMMANUEL ATSU <zioncodex@gmail.com>
2023-12-03 19:54:31 +0100

    commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use \App\Mail\SendMail;
+use App\Mail\SendMail;
 use Mail;
 
 class MailController extends Controller
