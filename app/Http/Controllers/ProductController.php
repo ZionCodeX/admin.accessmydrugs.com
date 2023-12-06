b7c48f35517052edf1d510c79a8465eef1728326
EMMANUEL ATSU <zioncodex@gmail.com>
2023-12-06 18:46:27 +0100

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -477,8 +477,8 @@ class ProductController extends Controller
         $data['product_sub_category1_name'] = DB::table('products_category')->where('category_slug',$request->product_sub_category1)->first("category_name");
         $data['product_sub_category2_name'] = DB::table('products_category')->where('category_slug',$request->product_sub_category2)->first("category_name");
         
-        $box[] = DB::table('products_category')->where('category_slug',$request->product_category)->first();
-dd($box[0]);
+        $box = DB::table('products_category')->where('category_slug',$request->product_category)->first();
+dd($box->category_name);
         //INSERT RECORDS TO PRODUCTS TABLE
         if((DB::table('products')->latest('seq')->first('seq')) == null){$seq = 0;}else{
         $seq = (int)(DB::table('products')->latest('seq')->first('seq')->seq) + 10;//record sequence update
