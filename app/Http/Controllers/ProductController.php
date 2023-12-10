4a149097e4ed6627d0dc9b60ba9de56a34e19743
EMMANUEL ATSU <zioncodex@gmail.com>
2023-12-10 23:30:27 +0100

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,7 +46,7 @@ class ProductController extends Controller
     {
 
         //$id = $request->id;
-        $datax = DB::table('products_export_1609_20')->get();
+        $datax = DB::table('products_export_1609_21')->get();
         $countx = 1;
 
         foreach ($datax as $record) {
