7edd2b4662ffa0df099cfa4c761480f3eecf21b2
EMMANUEL ATSU <zioncodex@gmail.com>
2024-05-12 18:57:47 +0100

    commit

--- resources/views/pages/orders_view_status_index.blade.php
+++ resources/views/pages/orders_view_status_index.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.base')
 
 
-@section('title', 'Users Record')
+@section('title', 'Order Record')
 
 
 @section('header_links')
@@ -40,44 +40,67 @@
     <thead>
         <tr>
             <th scope="col">#</th>
-            <th scope="col">Customer ID</th>
-            <th scope="col">Contact Details</th>
-            <th scope="col">Status</th>
-            <th scope="col">Created </th>
+            <th scope="col">Order Details</th>
+            <th scope="col">Product Details</th>
+            <th scope="col">Order Status</th>
+            <th scope="col">Date </th>
         </tr>
     </thead>
     <tbody>
 
 
 
-    @foreach ($users_x as $record)
+    @foreach ($orders as $record)
         <tr>
             <th scope="row">{{ $loop->iteration }}</th>
             
 
             <td>
-                <!--<h4>{{ $record->company_name ?? '' }}</h4>-->
-                <b>ID: </b>{{ $record->pid_user ?? '' }} <br>
+                <b>Order ID: </b>{{ $record->pid_order ?? '' }} <br>
+                <b>Customer Name: </b>{{ $record->first_name.' '.$record->last_name ?? 'NA' }} <br>
+                <b>Contact: </b>{{ $record->email.' | '.$record->phone ?? 'NA' }} <br>
+                <b>Location: </b>{{ $record->city ?? 'NA' }} 
             </td>
             
+
             <td>
-                <b>Name: </b>{{ $record->first_name.' '.$record->last_name ?? 'NA' }} <br>
-                <b>Email: </b>{{ $record->email ?? '' }} <br> 
-                <b>Phone: </b>{{ $record->phone ?? '' }}
+                <b>Product ID: </b>{{ $record->pid_order ?? 'NA' }} <br>
+                <b>Product Name: </b>{{ $record->first_name.' '.$record->last_name ?? 'NA' }} <br>
+                <!--<b> Qty: </b>1<br>-->
+                <b>Total Cost: </b>{{ $record->order_total_cost ?? 'NA' }} 
             </td>
+
             
             <td>
-                @if(empty($record->email_verified_at))
-                    <span class="label label-inline label-light-danger font-weight-bold">
-                        UN-ACTIVATED
+                @if(empty($record->status) || $record->status == null)
+                    <span class="label label-inline label-light-danger font-weight-bold" style="color: grey">
+                        Attempted
                     </span>
-                @else
-                    <span class="label label-inline label-light-success font-weight-bold">
-                        ACTIVATED
+                @elseif ($record->status == 'PAID')
+                    <span class="label label-inline label-light-success font-weight-bold" style="color: green">
+                        Paid
+                    </span>
+                @elseif ($record->status == 'PROCESSING')
+                    <span class="label label-inline label-light-success font-weight-bold" style="color: blue">
+                        Processing
+                    </span>
+
+                @elseif ($record->status == 'IN_TRANSIT')
+                    <span class="label label-inline label-light-success font-weight-bold" style="color: purple">
+                        In-Transit
+                    </span>
+                @elseif ($record->status == 'DELIVERED')
+                    <span class="label label-inline label-light-success font-weight-bold" style="color: yellow">
+                        Delivered
+                    </span>
+                @elseif ($record->status == 'CANCELLED')
+                    <span class="label label-inline label-light-success font-weight-bold" style="color: red">
+                        Cancelled
                     </span>
                 @endif
             </td>
             <td>{{ $record->created_at ?? '' }}</td>
+            <td>Details</td>
         </tr>
     @endforeach
 
